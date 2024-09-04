47aad8989a4bae829ba9bbd1057c30f5eb4b808f
LEE WOOJIN <24864@naver.com>
2024-09-04 15:19:19 +0900

    about me

--- woojin/woojin-portfolio/src/components/modules/Profile.jsx
+++ woojin/woojin-portfolio/src/components/modules/Profile.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import "../../css/profile.scss"
+import React from "react";
+
+import "../../css/profile.scss";
+
 function Profile(props) {
-    return (
-        <div id='profile-area'>
+  return (
+    <div id="profile-area">
+      <div>
+        <h2>About me</h2>
+        <div className="profile-box">
+          <div className="pf-img col-6">
+            <img src="/images/myp.jpg" alt="" />
+          </div>
+          <div className="pf-text col-6">
             <div>
-                <h2>
-                    About me
-                </h2>
-                <div className='pf-img'>
-                    <img src="" alt="" />
-                </div>
-                <div className='pf-text'></div>
+              <h3>이우진</h3>
+              <p className="introduce">
+                적극적으로 효율적 웹 환경을 구현하는 개발자
+              </p>
+              <p>DOB : 1999.10.07</p>
+              <p>Email : 24864@naver.com</p>
+              <p>
+                Github :{" "}
+                <a href="https://github.com/woojin999" target="_blank">
+                  woojin999
+                </a>
+              </p>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
