68fa7a65fe94980b25e2bb726959cd410ed4f250
LEE WOOJIN <24864@naver.com>
2024-09-06 17:01:34 +0900

    pf

--- woojin/woojin-portfolio/src/components/modules/Skill.jsx
+++ woojin/woojin-portfolio/src/components/modules/Skill.jsx
@@ -1,13 +1,60 @@
-import React from 'react';
+import React from "react";
 
 import "../../css/skill.scss";
 
 function Skill(props) {
-    return (
-        <div id='skill-area'>
-            
+  return (
+    <div id="skill-area">
+      <div>
+        <h2>Skill</h2>
+        <p>현재까지 배우고 사용 해봤던 기술입니다</p>
+        <div className="skill-box">
+          <ul>
+            <li>
+              <img src="/images/skill/react.png" alt="" />
+            </li>
+            <li>
+              <img src="/images/skill/html.png" alt="html" />
+            </li>
+            <li>
+              <img src="/images/skill/css.png" alt="css" />
+            </li>
+            <li>
+              <img src="/images/skill/javascript.png" alt="js" />
+            </li>
+            <li>
+              <img src="/images/skill/sass.png" alt="sass" />
+            </li>
+            <li>
+              <img src="/images/skill/jquery.png" alt="jquary" />
+            </li>
+            <li>
+              <img src="/images/skill/nodejs.png" alt="node" />
+            </li>
+            <li>
+              <img src="/images/skill/java.png" alt="java" />
+            </li>
+            <li>
+              <img src="/images/skill/spring.png" alt="spring" />
+            </li>
+            <li>
+              <img src="/images/skill/github.png" alt="github" />
+            </li>
+            <li>
+              <img src="/images/skill/vercel.png" alt="vercel" />
+            </li>
+            <li>
+              <img src="/images/skill/aws.png" alt="aws" />
+            </li>
+          </ul>
+          <ul className="snd-ul">
+          </ul>
+          <ul>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Skill;
