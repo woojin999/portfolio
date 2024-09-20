715a4e12d7aa2e04b3508d3dd4bacfb0e0f2e71c
LEE WOOJIN <24864@naver.com>
2024-09-20 15:19:40 +0900

    pt

--- woojin/woojin-portfolio/src/components/modules/HongDetail.jsx
+++ woojin/woojin-portfolio/src/components/modules/HongDetail.jsx
@@ -18,6 +18,31 @@ function HongDetail(props) {
         <div className="hcp-area">
           <div className="hcp-detail-box">
             <h4>HongRuizhen Clone Project</h4>
+            <div className="desc-wrap">
+            <div className="detail-wrap">
+              <div className="col-7 dt-img">
+                <ul>
+                  <li>
+                    <img src="/images/plants/signup.png" alt="signup" />
+                  </li>
+                  <li>
+                    <img src="/images/plants/login.png" alt="login" />
+                  </li>
+                </ul>
+              </div>
+              <div className="col-5 dt-desc">
+                <p>회원가입 / 로그인</p>
+                <ul>
+                  <li>아이디, 패스워드, 이메일 정규식 사용</li>
+                  <li>회원가입 성공 시 로컬 스토리지에 저장</li>
+                  <li>
+                    로그인 페이지에서 로컬 스토리지의 데이터 확인 후 일치하면
+                    메인 페이지 이동
+                  </li>
+                </ul>
+              </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
