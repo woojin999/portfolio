78bcfe21cc7a1de211f1e208f5113c5f0b70cc6b
LEE WOOJIN <24864@naver.com>
2024-09-20 17:53:13 +0900

    font

--- woojin/woojin-portfolio/src/components/modules/banner.jsx
+++ woojin/woojin-portfolio/src/components/modules/banner.jsx
@@ -11,7 +11,7 @@ function Banner(props) {
           <br />
           Works Place
         </h2>
-        <p>안녕하세요!</p>
+        <p>안녕하세요!!</p>
         <p>웹 개발자 이우진의 포트폴리오 입니다.</p>
         <p className="scroll-down">scroll down</p>
         <div className="scroll-bar"></div>
